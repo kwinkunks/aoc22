6bc4b1d7bdcb67de49fc451a276233a3c0259bd9
Matt Hall <kwinkunks@gmail.com>
2022-12-07 13:36:19 +0000

    sudden enlightenment: unique names -facepalm

--- day07.py
+++ day07.py
@@ -28,7 +28,7 @@ $ ls
 
 INPUT = RAW.splitlines()
 
-def parse(data: str) -> dict:
+def parse(data: list[str]) -> dict:
     """Parse the directory listings, accumulating the sizes of directories.
     Directories are marked in the data by a line starting with 'dir' followed
     by the name of the directory. Files are denoted by a line containing the
@@ -37,14 +37,13 @@ def parse(data: str) -> dict:
     Returns a dictionary of the total accumulated size of each directory,
     including all its subdirectories.
     """
-    path = []
-    sizes = {}
+    path: list[str] = []
+    sizes: dict = {}
     for line in data:
-        if line.startswith('$ cd'):
-            if line == '$ cd ..':
-                path.pop()
-            else:
-                path.append(line[5:])
+        if line == '$ cd ..':
+            path.pop()
+        elif line.startswith('$ cd'):
+            path.append(line[5:])
         elif line.startswith('$ ls'):
             continue
         elif line.startswith('dir'):
@@ -54,17 +53,21 @@ def parse(data: str) -> dict:
             sizes[tuple(path)] = sizes.get(tuple(path), 0) + int(size)
     return sizes
 
-def get_dir_sizes(sizes):
+
+def get_dir_sizes(sizes: dict) -> dict:
     """Given a dictionary of directory sizes, return a dictionary of the
     sizes of each directory, including its subdirectories.
     """
-    dirs = {}
+    dirs: dict = {}
     for path, size in sizes.items():
-        for dir in path:
-            dirs[dir] = dirs.get(dir, 0) + size
+        p = list(path)
+        while p:
+            dirs[tuple(p)] = dirs.get(tuple(p), 0) + size
+            p.pop()
     return dirs
 
-def sum_small_dirs(dirs, limit=100_000):
+
+def sum_small_dirs(dirs: dict, limit: int=100_000) -> int:
     """Given a dictionary of directory sizes, return the total size of those directories
     whose size is less than or equal to the given limit.
     """
@@ -72,9 +75,28 @@ def sum_small_dirs(dirs, limit=100_000):
 
 assert sum_small_dirs(get_dir_sizes(parse(INPUT))) == 95437
 
+def find_smallest(dirs: dict, total: int, required: int) -> int:
+    """Given a dictionary of directory sizes, return the size of the smallest
+    directory that is larger than the given required size.
+    """
+    freespace = total - dirs[('/',)]
+    want = required - freespace
+    smallest = total
+    for _, size in get_dir_sizes(parse(data)).items():
+        if (size > want) and (size < smallest):
+            smallest = size
+    assert smallest > want
+    return smallest
+
+
 if __name__ == "__main__":
 
     with open('data/day07.txt') as f:
         data = f.read().splitlines()
 
-    print(sum_small_dirs(get_dir_sizes(parse(data))))
+    part1 = sum_small_dirs(get_dir_sizes(parse(data)))
+    print('Part 1:', part1)
+    assert part1 == 1447046
+
+    part2 = find_smallest(get_dir_sizes(parse(data)), 70000000, 30000000)
+    print('Part 2:', part2)
