288e550aa07baf8d9d51bbcb367600e9a23d3ef7
Matt Hall <kwinkunks@gmail.com>
2022-12-09 14:35:50 +0000

    getting there

--- day09.py
+++ day09.py
@@ -2,8 +2,6 @@
 Advent of Code 2022
 Day 9
 """
-from typing import Self
-
 RAW = """R 4
 U 4
 L 3
@@ -26,43 +24,61 @@ def sign(n: complex) -> complex:
     return (n.real / abs(n.real)) + (n.imag / abs(n.imag)) * 1j
 
 class Rope:
-    def __init__(self) -> None:
-        self.head = complex(0, 0)
-        self.tail = complex(0, 0)
-        self.history = {self.tail}
+    def __init__(self, n=2) -> None:
+        self.knots = [complex(0, 0) for _ in range(n)]
+        self.history = {self.knots[-1]}
 
     def __repr__(self) -> str:
         return f'Rope(head={self.head}, tail={self.tail})'
 
-    def move(self, direction: str, distance: int) -> Self:
+    @property
+    def head(self) -> complex:
+        return self.knots[0]
+
+    @property
+    def tail(self) -> complex:
+        return self.knots[-1]
+
+    def move(self, direction: str, distance: int):
         for _ in range(distance):
-            self.head += DIRECTIONS[direction]
-            self = self.move_tail()
+            knots = self.knots.copy()
+            for i, (u, v) in enumerate(zip(knots, knots[1:])):
+                u_, v_ = move_knots(u, v, direction)
+                knots[i], knots[i+1] = u_, v_
+            self.knots = knots
+            self.history.add(self.tail)
         return self
 
-    def move_tail(self) -> Self:
-        """Move the tail according to the following rules: if the head is
-        touching the tail, do nothing; if the head is adjacent to the tail,
-        do nothing. If the head is two units away from the tail,
-        move the tail one unit closer to the head. If the head is more than two
-        units away from the tail, move the tail one diagonal unit closer to
-        the head."""
-        if (L := abs(self.head - self.tail)) < 2:
-            pass
-        elif L == 2:
-            self.tail += (self.head - self.tail) / 2
-        elif L > 2:
-            self.tail += sign(self.head - self.tail)
-        self.history.add(self.tail)
-        return self
+def move_knots(u, v, direction: str):
+    """Move knot v according to the rules."""
+    if (L := abs(u - v)) == 2:
+        v += (u - v) / 2
+    elif L > 2:
+        v += sign(u - v)
+    return u + DIRECTIONS[direction], v
 
-def play(data: list[tuple[str, int]]) -> int:
-    rope = Rope()
+def play(data: list[tuple[str, int]], n=2) -> int:
+    rope = Rope(n=n)
     for direction, distance in data:
         rope.move(direction, distance)
+        print(rope)
     return len(rope.history)
 
-assert(play(INPUT) == 13)
+# assert(play(INPUT) == 13)
+
+print("length 10: ", play(INPUT, n=10))
+
+PART2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+
+# print(play(list(parse(PART2)), n=10))
+# assert play(list(parse(PART2)), n=10) == 36
 
 
 if __name__ == "__main__":
@@ -70,6 +86,6 @@ if __name__ == "__main__":
     with open('data/day09.txt') as f:
         moves = list(parse(f.read()))
 
-    part1 = play(moves)
-    print('Part 1:', part1)
-    assert part1 == 6175
+    # part1 = play(moves)
+    # print('Part 1:', part1)
+    # assert part1 == 6175
